1711f8e49fa8db0595fc57aaa4e5d7a26eeb3e3c
王刚 <wanggang@zxisl.com>
2014-12-02 11:28:37 +0800

    重构项目，移除不必要依赖关系

---
+++ sentiment-core/src/test/java/zx/soft/sent/core/tsdb/GatherQueueReportTest.java
@@ -0,0 +1,12 @@
+package zx.soft.sent.core.tsdb;
+
+import org.junit.Test;
+
+public class GatherQueueReportTest {
+
+	@Test
+	public void test() {
+		//
+	}
+
+}
---
+++ sentiment-spider/src/test/java/zx/soft/sent/spider/dns/CacheDNSTest.java
@@ -0,0 +1,12 @@
+package zx.soft.sent.spider.dns;
+
+import org.junit.Test;
+
+public class CacheDNSTest {
+
+	@Test
+	public void test() {
+		System.out.println("Not yet implemented");
+	}
+
+}
