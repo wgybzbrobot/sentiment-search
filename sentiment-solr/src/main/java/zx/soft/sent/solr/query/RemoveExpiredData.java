754f13896903a398b5a084faf1a507cac30d66ce
王刚 <wanggang@zxisl.com>
2015-11-12 16:29:26 +0800

    修改定时删除数据模块

--- sentiment-solr/src/main/java/zx/soft/sent/solr/query/RemoveExpiredData.java
+++ sentiment-solr/src/main/java/zx/soft/sent/solr/query/RemoveExpiredData.java
@@ -1,8 +1,5 @@
 package zx.soft.sent.solr.query;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,8 +12,8 @@ import zx.soft.utils.time.TimeUtils;
  *
  * 注意：每天晚上执行一次，否则第二天更新的重复数据不会被保存到Solr中。
  *
- * 1、删除1周前的微博数据
- * 2、删除1个月前的其他舆情数据
+ * 1、删除2周前的微博数据
+ * 2、删除2个月前的其他舆情数据
  * 3、清空Redis中的缓存id数据（sent.cache.records）
  *
  * 运行目录：/home/zxdfs/run-work/remove
@@ -34,36 +31,34 @@ public class RemoveExpiredData {
 	 * 主函数
 	 */
 	public static void main(String[] args) {
-		remove();
+		remove(args[0]);
 	}
 
-	public static void runTimer() {
-		Timer timer = new Timer();
-		timer.schedule(new RemoveTimer(), 0, 86400_000L * 7);
-	}
-
-	public static class RemoveTimer extends TimerTask {
-
-		@Override
-		public void run() {
-			remove();
+	public static void remove(String type) {
+		if ("all".equalsIgnoreCase(type)) {
+			removeSentimentData();
+			removeRedisReplicationData();
+		} else if ("redis".equalsIgnoreCase(type)) {
+			removeRedisReplicationData();
 		}
-
+		logger.info("Finish Removing expired data ...");
 	}
 
-	public static void remove() {
+	private static void removeSentimentData() {
 		logger.info("Start Removing expired data ...");
 		QueryCore queryCore = new QueryCore();
-		// 删除七天之前的微博数据
+		// 删除15天之前的微博数据
 		String end = TimeUtils.transToSolrDateStr(System.currentTimeMillis() - 15 * 86400_000L);
 		String query = "platform:3 AND lasttime:[1970-01-01T00:00:00Z TO " + end + "]";
 		queryCore.deleteQuery(query);
-		// 删除一个月之前的其他舆情数据
+		// 删除2个月之前的其他舆情数据
 		end = TimeUtils.transToSolrDateStr(System.currentTimeMillis() - 60 * 86400_000L);
 		query = "lasttime:[1970-01-01T00:00:00Z TO " + end + "]";
 		queryCore.deleteQuery(query);
 		queryCore.close();
-		logger.info("Finish Removing expired data ...");
+	}
+
+	private static void removeRedisReplicationData() {
 		// 情况Redis中的ID去重数据
 		logger.info("Start Removing redis-replication data ...");
 		RedisMQ redisMQ = new RedisMQ();
